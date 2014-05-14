a9534c2b4acb64757e3d2c7b4a0e6d2d8df54003
Matt Highland <ichibangaijin77@gmail.com>
2014-05-14 15:07:50 -0700

    improved jquery

--- js/main.js
+++ js/main.js
@@ -2,20 +2,25 @@ $(document).ready(function () {
         $("#supercharged").click(function () {
             $("#main-image").hide(0, showPic);
         });
-        $(".boxSlider h2").mouseover(function () {
-            $(".boxSlider h2").animate({
+        $(".boxSlider").mouseenter(function () {
+            var target = $(this).find('p');
+            var h2 = $(this).find('h2');
+            h2.animate({
                 "margin-top": "10px"
-            }, 700, showPara);
+            }, 700, function(){showPara(target);});
         });
     });
 
-        function showPic() {
-            $("#main-image-alternate").show();
-            $("#supercharged").hide();
-            $("#shell, #colorScreen, #memory, #floppyDrive").fadeIn(3000);
-        }
 
-
-function showPara() {
-    $(".boxSlider p").show(800);
+function showPic(){
+    $("#main-image-alternate").show();
+    $("#supercharged").hide();
+    $("#shell, #colorScreen, #memory, #floppyDrive").fadeIn(3000);
 }
+
+
+function showPara(target) {
+    target.show(800);
+}
+
+
